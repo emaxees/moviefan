c5f322616c0bdb00c98730b6d2530f1f3d63c2d2
maximiliano.escobar@rga.com <>
2019-11-20 18:22:54 -0300

    Add linter

--- components/nav.js
+++ components/nav.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+    { href: 'https://zeit.co/now', label: 'ZEIT' },
+    { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+].map((link) => ({
+    ...link,
+    key: `nav-link-${link.href}-${link.label}`,
+}));
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
-    </ul>
+    <nav>
+        <ul>
+            <li>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </li>
+            {links.map(({ key, href, label }) => (
+                <li key={key}>
+                    <a href={href}>{label}</a>
+                </li>
+            ))}
+        </ul>
 
-    <style jsx>{`
+        <style jsx>
+            {`
       :global(body) {
         margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
@@ -49,8 +50,9 @@ const Nav = () => (
         text-decoration: none;
         font-size: 13px;
       }
-    `}</style>
-  </nav>
-)
+    `}
+        </style>
+    </nav>
+);
 
-export default Nav
+export default Nav;
