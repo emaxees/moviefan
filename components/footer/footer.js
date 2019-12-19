fae24240bdbbbe42ba9a0f8b5aae625563825e53
manuel <manuel@sahores.com>
2019-12-18 22:35:42 -0300

    wip

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,49 +1,45 @@
 import React from 'react';
 import { Subtitle } from 'components';
+import Link from 'next/link';
+import {
+  Container, Row, Col, Visible,
+} from 'react-grid-system';
+
+
+const containerStyles = {
+    padding: '2rem 4rem',
+    color: '#494c62',
+  };
 
 const Footer = () => (
-  <div className="container">
-    <div className="first-row">
-      <Subtitle>THE MOVIE BOX</Subtitle>
-      <ul>
-        <li>About</li>
-        <li>Movies</li>
-        <li>Rating</li>
-        <li>Contact</li>
-      </ul>
-    </div>
-    <div className="second-row">
-      <ul>
-        <li className="facebook">facebook</li>
-        <li className="pinterest">pinterest</li>
-        <li className="twitter">twitter</li>
-        <li className="linkedin">linkedin</li>
-      </ul>
-    </div>
-    <style jsx>
-      {`
-        .container {
-          background-color: var(--global-color-white);
-          display: flex;
-          flex-direction: column;
-          padding: 2rem 4rem;
-          color: var(--global-color-grey);
-        }
+    <Container fluid style={containerStyles}>
+        <Row>
+            <Col lg={6}>
+                <Subtitle>THE MOVIE BOX</Subtitle>
+            </Col>
+            <Col lg={6}>
+                <Row justify="end">
+                    <span>About</span>
+                    <span>Movies</span>
+                    <span>Rating</span>
+                    <span>Contact</span>
+                </Row>
+                <Row align="end" justify="end">
+                    <div className="social">
+                        <Link href="/"><a className="facebook">facebook</a></Link>
+                        <Link href="/" className="pinterest"><a>pinterest</a></Link>
+                        <Link href="/" className="twitter"><a>twitter</a></Link>
+                        <Link href="/" className="linkedin"><a>linkedin</a></Link>
+                    </div>
+                </Row>
+            </Col>
+        </Row>
 
-        .first-row {
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: center;
-        }
-        .second-row {
-          & ul {
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-end;
-            align-items: center;
-          }
-          & li {
+        <style jsx>
+            {`
+        .social {
+          background-color: yellow;
+          & > * {
             width: 26px;
             height: 30px;
             margin: 3px;
@@ -53,27 +49,28 @@ const Footer = () => (
             color: var(--global-color-grey-light);
             cursor: pointer;
 
-            & :hover {
+            &:hover {
               opacity: 0.5;
               transition: 0.5s linear opacity;
             }
-          }
-          & .facebook {
-            background-position: 0px -1px;
-          }
-          & .pinterest {
-            background-position: -57px -1px;
-          }
-          & .twitter {
-            background-position: -114px -1px;
-          }
-          & .linkedin {
-            background-position: -171px -1px;
+
+            &.facebook {
+              background-position: 0px -1px;
+            }
+            &.pinterest {
+              background-position: -57px -1px;
+            }
+            &.twitter {
+              background-position: -114px -1px;
+            }
+            &.linkedin {
+              background-position: -171px -1px;
+            }
           }
         }
       `}
-    </style>
-  </div>
+        </style>
+    </Container>
 );
 
 export default Footer;
